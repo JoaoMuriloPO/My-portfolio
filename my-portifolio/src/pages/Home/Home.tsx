18ce75bcb68beafc6ee7f15899a21db4bdefbd7e
Joao Murilo <j.murilo.mobile@gmail.com>
2025-02-15 03:28:37 -0300

    "feat: finished project"

---
+++ my-portifolio/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+import Hero from "../Home/section/Hero/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
